da4e231037ee88bacb960454ac2f00e8999de57c
andrew kim <andrewk1216@gmail.com>
2020-01-05 23:10:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 // Code your solution here!
 
+const printString = (string) => {
+  if (string.length === 1) {
+    console.log(string)
+  }
+  else {
+    return printString(string.slice(0, string.length))
+  }
+}
+
+const reverseString = (string) => {
+  if (string.length === 1) {
+    console.log(string);
+  }
+  else {
+    return reverseString(string[0])
+  }
+}
