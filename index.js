12b9e076252fca6ad38df77239f0f7b92adb2b04
andrew kim <andrewk1216@gmail.com>
2020-01-05 23:11:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 // Code your solution here!
 
 const printString = (string) => {
-  if (string.length === 1) {
-    console.log(string)
-  }
-  else {
+  console.log(string[0])
+  if (string.length > 1) {
     return printString(string.slice(0, string.length - 1))
   }
 }
